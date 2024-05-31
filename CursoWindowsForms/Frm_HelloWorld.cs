7c1c96148d292d8b35d7f3dd8a94407b8db6292d
Gilberto Soares <gilbertonsoarew@gmail.com>
2024-05-31 11:21:22 -0300

    Adicionar arquivos de projeto.

---
+++ CursoWindowsForms/Frm_HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_HelloWorld : Form
+    {
+        public Frm_HelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Sair_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Btn_ModificaLabel_Click(object sender, EventArgs e)
+        {
+            lbl_Titulo.Text = Txt_ConteudoLabel.Text;
+        }
+    }
+}
