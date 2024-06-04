cc10111eecf48aa7d73afe78c431f9c316340233
Gilberto Soares <gilbertonsoarew@gmail.com>
2024-06-04 10:25:36 -0300

    User Control Implementado.

---
+++ CursoWindowsForms/Frm_ValidaCPF2_UC.cs
@@ -0,0 +1,60 @@
+﻿using CursoWindowsFormsBiblioteca;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF2_UC : UserControl
+    {
+        public Frm_ValidaCPF2_UC()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            string vConteudo = Msk_CPF.Text
+                .Replace(".", "")
+                .Replace("-", "")
+                .Trim();
+
+
+            if (vConteudo == "")
+            {
+                MessageBox.Show("O Campo CPF não pode ser vazio.", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (vConteudo.Length != 11)
+            {
+                MessageBox.Show("É necessário incluir todos os dígitos do CPF.", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (MessageBox.Show("Você deseja realmente validar o CPF?", "Mensagem de Validação", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    bool validaCpf = false;
+                    validaCpf = Cls_Uteis.Valida(Msk_CPF.Text);
+                    if (validaCpf == true)
+                    {
+                        MessageBox.Show("CPF Válido", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("CPF Inválido", "Mensagem de Validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CursoWindowsForms/Frm_ValidaCPF_UC.cs
@@ -0,0 +1,37 @@
+﻿using CursoWindowsFormsBiblioteca;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF_UC : UserControl
+    {
+        public Frm_ValidaCPF_UC()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Lbl_Resultado.Text = "";
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            bool validaCpf = false;
+            validaCpf = Cls_Uteis.Valida(Msk_CPF.Text);
+            if (validaCpf == true)
+            {
+                Lbl_Resultado.Text = "Cpf Válido";
+                Lbl_Resultado.ForeColor = Color.ForestGreen;
+            }
+            else
+            {
+                Lbl_Resultado.Text = "Cpf Inválido";
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+        }
+    }
+}
